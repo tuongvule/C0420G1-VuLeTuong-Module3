40af95f6d10d702a9702c47405ed9d57589a5534
tuongvule <tuongvule@gmail.com>
2020-07-31 00:12:55 +0700

    hello friday 00:12

---
+++ 9_JSP - JAVA SERVLET/practice_java-web-currency-converter/src/Servlet_Currency_Converter.java
@@ -0,0 +1,10 @@
+@javax.servlet.annotation.WebServlet(name = "Servlet_Currency_Converter")
+public class Servlet_Currency_Converter extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+
+    }
+}
