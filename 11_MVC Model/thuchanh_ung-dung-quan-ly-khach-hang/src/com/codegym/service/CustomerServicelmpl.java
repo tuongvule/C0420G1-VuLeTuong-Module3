931ce4aae51af2cfa9cf6f14b8b46ab6de20871a
tuongvule <tuongvule@gmail.com>
2020-08-03 08:10:48 +0700

    hello MONDAY 8:10

---
+++ 11_MVC Model/thuchanh_ung-dung-quan-ly-khach-hang/src/com/codegym/service/CustomerServicelmpl.java
@@ -0,0 +1,45 @@
+package com.codegym.service;
+
+import com.codegym.model.Customer;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class CustomerServicelmpl implements CustomerService {
+    private static Map<Integer, Customer> customers;
+    static {
+        customers = new HashMap<>();
+        customers.put(1,new Customer(1,"Kim","kim@gmail.com","Bình Sa"));
+        customers.put(2,new Customer(1,"Hiền","hien@gmail.com","Bình Sa"));
+        customers.put(3,new Customer(1,"Ngân","ngan@gmail.com","Bình Sa"));
+        customers.put(4,new Customer(1,"Tiến","tien@gmail.com","Bình Sa"));
+        customers.put(5,new Customer(1,"Phước","phuoc@gmail.com","Bình Sa"));
+        customers.put(6,new Customer(1,"Tường","tuong@gmail.com","Bình Sa"));
+    }
+    @Override
+    public List<Customer> findAll() {
+        return new ArrayList<>(customers.values());
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customers.put(customer.getId(), customer);
+    }
+
+    @Override
+    public Customer findById(int id) {
+        return customers.get(id);
+    }
+
+    @Override
+    public void update(int id, Customer customer) {
+        customers.put(id,customer);
+    }
+
+    @Override
+    public void remove(int id) {
+        customers.remove(id);
+    }
+}
--- 9_JSP - JAVA SERVLET/java-web-mvc/web/index.jsp
+++ 9_JSP - JAVA SERVLET/java-web-mvc/web/index.jsp
@@ -1,8 +1,8 @@
 <%--
   Created by IntelliJ IDEA.
   User: Admin
-  Date: 7/25/2020
-  Time: 8:52 AM
+  Date: 8/2/2020
+  Time: 9:14 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
